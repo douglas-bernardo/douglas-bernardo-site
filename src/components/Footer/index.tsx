abb694bcee4df1f9019dd243838828806439fd4b
Douglas Bernardo <jkdouglas21@gmail.com>
2022-08-08 10:47:55 -0300

    refactor: added social media links on Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,28 +10,25 @@ export function Footer() {
     <footer className={styles.footerContainer}>
       <div className={styles.footerContent}>
         <nav>
-          <Link href={''}>
-            <a href="">
-              <RiGithubLine color="#121214" />
-            </a>
-          </Link>
-          <Link href={''}>
-            <a href="">
-              <RiInstagramLine color="#121214" />
-            </a>
-          </Link>
-          <Link href={''}>
-            <a href="">
-              <RiTwitterLine color="#121214" />
-            </a>
-          </Link>
-          <Link href={''}>
-            <a href="">
-              <FiLinkedin color="#121214" />
-            </a>
-          </Link>
-        </nav>
+          <a href="https://github.com/douglas-bernardo" target="_blank">
+            <RiGithubLine color="#121214" />
+          </a>
 
+          <a href="https://www.instagram.com/douglasb.dev/" target="_blank">
+            <RiInstagramLine color="#121214" />
+          </a>
+
+          <a href="https://twitter.com/douglasbdev" target="_blank">
+            <RiTwitterLine color="#121214" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/douglas-bernardo/"
+            target="_blank"
+          >
+            <FiLinkedin color="#121214" />
+          </a>
+        </nav>
         <div>&copy; 2022 - present Douglas Bernardo. All Rights Reserved.</div>
       </div>
     </footer>
